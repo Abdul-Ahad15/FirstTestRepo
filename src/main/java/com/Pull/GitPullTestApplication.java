fb6395132101d554b062f221fb3a918cdd75479a
mohammedabdulas <mohammedabdulas@STGIDT-CI3-11.stg.com>
2022-07-19 16:19:51 +0530

    test

---
+++ src/main/java/com/Pull/GitPullTestApplication.java
@@ -0,0 +1,13 @@
+package com.Pull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPullTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPullTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Pull/GitPullTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Pull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPullTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
